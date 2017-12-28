d9626d276ef27f4cc0047b022e20e4dd1bfd9aba
Andreas Johansson <aj854@outlook.com>
2017-12-28 15:50:40 +0100

    Övning 10.4 avklarad.

--- ovning10_4.c
+++ ovning10_4.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 #define MAX_LINE 1000
 
@@ -33,6 +35,41 @@ int fread_line(char *a,int n,FILE *f)
 	return 1;
 }
 
+FILE *open_file(char *text,char *kommando)
+{
+	printf("%s",text);
+	char namn[MAX_LINE];
+	fread_line(namn,MAX_LINE,stdin);
+	FILE *fil = fopen(namn,kommando);
+	return fil;
+}
+
 int main(void)
 {
-	char fileName[MAX_LINE];
+	FILE *infil = open_file("Logfilen: ","r");
+	char rad[MAX_LINE],namn[MAX_LINE] = "";
+	int sum = 0;
+	while (fread_line(rad,MAX_LINE,infil))
+	{
+		char delNamn[MAX_LINE],delNamn2[MAX_LINE] = "";
+		int i = 0,k,min,delSum = 0;
+		while (sscanf(rad+i,"%s%n",delNamn,&k) == 1 && !isdigit(delNamn[0]))
+		{
+			strcat(delNamn2,delNamn);
+			i += k;
+		}
+		while (sscanf(rad+i,"%d%n",&min,&k) == 1)
+		{
+			delSum += min;
+			i += k;
+		}
+		if (delSum > sum)
+		{
+			sum = delSum;
+			strcpy(namn,delNamn2);
+		}
+	}
+	fclose(infil);
+	printf("Personen som har varit inloggad mest på datorerna är %s, sammanlagt har %s varit inloggad i %d minuter.\n",namn,namn,sum);
+	return 0;
+}
