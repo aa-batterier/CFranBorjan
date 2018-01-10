d70ac730377b69d94f23c6d77e6e5395f1c38933
Andreas Johansson <aj854@outlook.com>
2018-01-10 10:36:14 +0100

    Uppgift 14.2 avklarad.

---
+++ kap14/uppgift14_2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+#define N 10
+#define T 10000.0
+
+_Bool ar_sorterat(struct list *l)
+{
+	for (int i = 1; i < size(l); i++)
+	{
+		double *p0 = get(l,i-1),*p1 = get(l,i);
+		if (*p0 > *p1)
+			return 0;
+	}
+	return 1;
+}
+
+int main(void)
+{
+	struct list *l = new_list();
+	double tal;
+	printf("Skriv in tal:\n");
+	while (scanf("%lf",&tal) == 1)
+	{
+		double *p = (double*)malloc(sizeof(double));
+		*p = tal;
+		add_last(l,p);
+	}
+	if (ar_sorterat(l))
+		printf("Fältet är sorterat.\n");
+	else
+		printf("Fältet är inte sorterat.\n");
+	exit(0);
+}
