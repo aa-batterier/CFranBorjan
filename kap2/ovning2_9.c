29a543506939e607a1028f3f815e6b05278aba1c
Andreas Johansson <aj854@outlook.com>
2019-07-20 11:54:08 +0200

    Small fix to the algorithm in kap2/ovning2_9.c.

--- kap2/ovning2_9.c
+++ kap2/ovning2_9.c
@@ -6,7 +6,7 @@ int main(void)
 	printf("Tiden det tog programmet att köra i sekunder: ");
 	scanf("%d",&tid);
 	tim = tid/3600;
-	min = min/60;
+	min = tim/60;
 	sek = tid;
 	printf("Antal timmar blev %d.\nAntal minuter blev %d.\nAntal sekunder blev %d.\n",tim,min,sek);
 	return 0;
